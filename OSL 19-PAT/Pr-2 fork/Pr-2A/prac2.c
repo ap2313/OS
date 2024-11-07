7a4fa0ccb1f439d6d1845bd1d9fca616c597d366
ap2313 <168991518+ap2313@users.noreply.github.com>
2024-11-07 22:15:34 +0530

    ascending.c

--- OSL 19-PAT/Pr-2 fork/Pr-2A/prac2.c
+++ OSL 19-PAT/Pr-2 fork/Pr-2A/prac2.c
@@ -1,83 +1,77 @@
 #include<unistd.h>
-#include<sys/types.h>
 #include<stdio.h>
-void asc(int*, int sz);
-void dsc(int*, int sz);
+#include<sys/types.h>
+
+void asc(int*,int sz);
+void dsc(int*,int sz);
 int main()
 {
- pid_t pid;
- int *a,sz,i;
- printf("enter the size of the array\n");
- scanf("%d",&sz);
- a=(int*)malloc(sz*sizeof(int));
- printf("enter element of %d size array\n",sz);
-	for(i=0;i<sz;i++)
-	{
-		printf("a[%d]:",i);
-		scanf("%d",&a[i]);
-	}
-	pid=fork();
-	switch(pid)
-	{
-		case -1:
-		printf("Error in fork\n");		
-		exit(-1);	
-		case 0:
-		printf("child process\n");	
-		asc(a,sz);
-		exit(0);	
-		default:
-		wait (NULL);
-		printf("Parent process\n");
-		dsc(a,sz);
-		exit(0);	
-	}
-return 0;
-}
-void asc(int *a, int sz)
-{
-int i,j,temp;
-for(i=0;i<sz;i++)
-	{
-	for(j=i+1;j<sz;j++)
-		{
-			if(a[i]>a[j])
-			{			
-			temp=a[i];
-			a[i]=a[j];
-			a[j]=temp;		
-		}	
-	}
-}
-printf("sorted array elements in ascending order are\n");
-for(i=0;i<sz;i++)
-	{
-	printf("%d\t",a[i]);		
-	}
-printf("\n");
-}
+    pid_t pid;
+    int *a,sz,i;
 
-void dsc(int *a, int sz)
-{
-int i,j,temp;
-for(i=0;i<sz;i++)
-	{
-	for(j=i+1;j<sz;j++)
-		{
-			if(a[i]<a[j])
-			{			
-			temp=a[i];
-			a[i]=a[j];
-			a[j]=temp;		
-		}	
-	}
+    printf("size of array");
+    scanf("%d",&sz);
+    a=(int*)malloc(sizeof(int));
+    printf("enter element of%d size array",sz);
+     for(i=0;i<sz;i++){
+        printf("a[%d]",i);
+        scanf("%d",&a[i];)
+     }
+     pid=fork();
+     switch(pid)
+     {
+        case -1;
+        printf("error in fork");
+        exit(-1);
+        case 0;
+        printf("child process");
+        asc(a,sz);
+        exit(0);
+        printf("parentprocess");
+        dsc(a,sz);
+        exit(0);
+     }
+    return 0;
 }
-printf("sorted array elements in descending order are\n");
-for(i=0;i<sz;i++)
-	{
-	printf("%d\t",a[i]);		
-	}
-printf("\n");
+void asc(int *a ,int sz)
+{
+    int i, j ,temp;
+    for(i=0;i<sz;i++){
+        for(j=i+1;j<sz;j++){
+            if(a[i]>a[j]){
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("sorted array in asc");
+    for(i=0;i<sz;i++)
+    {
+        printf("%d",a[i]);
+    }
+    printf("\n");
+}
+void dsc(int *a ,int sz)
+{
+    int i, j ,temp;
+    for(i=0;i<sz;i++){
+        for(j=i+1;j<sz;j++)
+        {
+            if(a[i]<a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("sorted array in asc");
+    for(i=0;i<sz;i++)
+    {
+        printf("%d",a[i]);
+    }
+    printf("\n");
 }
 
 
